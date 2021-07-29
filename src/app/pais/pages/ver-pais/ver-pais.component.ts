562a16b95034ab48ff166f509892a8939adf800f
Jonathan <jonulisanpar@gmail.com>
2021-07-29 13:49:19 -0500

    Seccion 8: SPA - PaisesApp
    Capitulo 113: Rxjs - SwitchMap
    En este capitulo se hiso uso de un operador de RXJS, este es SwitchMap.
    Este operador recibe un observable y en consecuencia dispara un segundo
    observable, este puede hacer uso de los resultados del primer
    observable.

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { switchMap } from "rxjs/operators";
+
 import { PaisService } from '../../services/pais.service';
 
 @Component({
@@ -15,15 +17,32 @@ export class VerPaisComponent implements OnInit {
     private paisService: PaisService) { }
 
   ngOnInit(): void {
-    this.activatedRoute.params
-      .subscribe( ({ id })  => {
-        console.log( id );
+    //OBSERVAR LINK, CONSULTAR PAIS: OPCION1
+    // this.activatedRoute.params
+    //   .subscribe( ({ id })  => {
+    //     console.log( id );
 
-        this.paisService.getPaisPorCodigoAlpha( id )
-          .subscribe( pais => {
-            console.log(pais);
-          })
-      })
+    //     this.paisService.getPaisPorCodigoAlpha( id )
+    //       .subscribe( pais => {
+    //         console.log(pais);
+    //       })
+    //   });
+
+    /*
+    El switch map recive los datos de un observable (en este caso el resultado de ActivatedRoute) y enseguida
+    puede dispara otro observable (this.paisService.getPaisPorCodigoAlpha( id )) que puede hacer uso de la informacion
+    optenida del primer observable; el observable disparadado (el segundo) hara la consulta del pais. 
+    Este operador (el switchMap) forma part de un pipe del observable de ActivatedRoute.
+    en la parte del subscribe del activedRoute, la respuesta del observable es la que retorna la consulta del observable
+    disparado (el segundo observable del SwitchMap) del switchMap, que es la informacion de pais consultada por codigo.
+    */
+    this.activatedRoute.params
+      .pipe(
+        switchMap( ({ id })  => this.paisService.getPaisPorCodigoAlpha( id ))
+      )
+      .subscribe( resp => {
+        console.log(resp)
+      });
   }
 
 }
